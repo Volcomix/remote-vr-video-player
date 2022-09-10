88be42a0b78d8dde458ca5ba79957dcf8efc9f56
Volcomix <Volcomix@users.noreply.github.com>
2022-09-10 16:45:09 +0200

    Bootstrap rendering with Three

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,66 @@ import {
   Experimental_CssVarsProvider as CssVarsProvider,
   StyledEngineProvider,
 } from '@mui/material/styles'
-import { useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
+import {
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  SphereGeometry,
+  VideoTexture,
+  WebGLRenderer,
+} from 'three'
 
 import VideoDropzone from 'features/VideoDropzone'
 
 import styles from './App.module.css'
 
+const width = 1920
+const height = 1080
+
 const App = () => {
   const [videoUrl, setVideoUrl] = useState<string>()
+  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null)
+  const renderer = useRef<WebGLRenderer | null>(null)
+
+  const init = useCallback(
+    (video: HTMLVideoElement | null) => {
+      if (!video || !canvas) {
+        renderer.current?.setAnimationLoop(null)
+        renderer.current = null
+        return
+      }
+
+      const scene = new Scene()
+      const camera = new PerspectiveCamera(70, width / height, 1, 2000)
+
+      const texture = new VideoTexture(video)
+
+      const leftGeometry = new SphereGeometry(500, 64, 32)
+      leftGeometry.scale(-1, 1, 1)
+
+      const leftUvs = leftGeometry.attributes.uv.array
+
+      for (let i = 0; i < leftUvs.length; i += 2) {
+        ;(leftUvs[i] as any) *= 0.5 // FIXME
+      }
+
+      const leftMaterial = new MeshBasicMaterial({ map: texture })
+
+      const leftMesh = new Mesh(leftGeometry, leftMaterial)
+      leftMesh.rotation.y = -Math.PI / 2
+      scene.add(leftMesh)
+
+      renderer.current = new WebGLRenderer({ canvas })
+      renderer.current.setSize(width, height)
+
+      renderer.current.setAnimationLoop(() => {
+        renderer.current?.render(scene, camera)
+      })
+    },
+    [canvas]
+  )
 
   return (
     <>
@@ -21,7 +73,17 @@ const App = () => {
             <VideoDropzone
               onDrop={(file) => setVideoUrl(URL.createObjectURL(file))}
             />
-            <video className={styles.video} src={videoUrl} controls />
+            {videoUrl && (
+              <>
+                <video
+                  className={styles.video}
+                  ref={init}
+                  src={videoUrl}
+                  controls
+                />
+                <canvas className={styles.canvas} ref={setCanvas} />
+              </>
+            )}
           </div>
         </CssVarsProvider>
       </StyledEngineProvider>
