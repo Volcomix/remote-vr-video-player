d32a51e13cf3d382350a9d0b2fc73f310d97a5a1
Volcomix <Volcomix@users.noreply.github.com>
2022-09-10 15:30:57 +0200

    Render selected video

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,25 @@ import {
   Experimental_CssVarsProvider as CssVarsProvider,
   StyledEngineProvider,
 } from '@mui/material/styles'
+import { useState } from 'react'
 
 import VideoDropzone from 'features/VideoDropzone'
 
 import styles from './App.module.css'
 
 const App = () => {
+  const [videoUrl, setVideoUrl] = useState<string>()
+
   return (
     <>
       <CssBaseline />
       <StyledEngineProvider injectFirst>
         <CssVarsProvider>
           <div className={styles.root}>
-            <VideoDropzone />
+            <VideoDropzone
+              onDrop={(file) => setVideoUrl(URL.createObjectURL(file))}
+            />
+            <video className={styles.video} src={videoUrl} controls />
           </div>
         </CssVarsProvider>
       </StyledEngineProvider>
